8b232cdbb1921200b7fe8a017ffb8c2bdd65ddc0
Simon Leiva <leivaprogramming@gmail.com>
2020-05-19 17:12:07 -0400

    changes on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :listings do
-    resources :bookings, only: [ :new, :create, :edit, :update, :show ]
+    resources :bookings, only: [ :new, :create, :edit, :update, :show, :index]
   end
 end
