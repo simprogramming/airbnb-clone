602f869179b46716fe0ac15c930cfa68bb92c3aa
Simon Leiva <leivaprogramming@gmail.com>
2020-05-19 11:05:56 -0400

    booking bug fix

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+  def show
+    @booking = Booking.find(params[:id])
+  end
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render 'new'
+    end
+  end
+  def new
+    @listing = Listing.find(params[:listing_id])
+    @booking = Booking.new()
+  end
+  private
+  def booking_params
+    params.require(:booking).permit(:title)
+  end
 end
