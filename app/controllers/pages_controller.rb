abddca8b4c3c0d6552018c22fcc53e1b9ebd9f49
Adrien Laudadio <adrien.laudadio.b@gmail.com>
2020-05-20 16:00:34 -0400

    qwergdtytg

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   end
 
   def manage
+    @requests = Booking.joins(:listing).where(listings: {user_id: current_user.id})
+  end
 
+  def confirm
   end
 end
